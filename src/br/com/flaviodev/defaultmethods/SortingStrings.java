ad875419e80279d3dc9cdd92720cdb52e8a776f1
flavio <fdsdev@gmail.com>
2018-01-09 21:36:31 -0200

    default methods on interfaces'

---
+++ src/br/com/flaviodev/defaultmethods/SortingStrings.java
@@ -0,0 +1,57 @@
+package br.com.flaviodev.defaultmethods;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class SortingStrings {
+
+	public static void main(String[] args) {
+
+		List<String> words = new ArrayList<String>();
+		words.add("updating");
+		words.add("and");
+		words.add("reviewing");
+		words.add("java");
+		words.add("knowledge");
+
+		Comparator<String> comparator = new SizeComparator();
+
+		// old way for sorting words
+		Collections.sort(words, comparator);
+		System.out.println(words);
+
+		// using default method sort from interface List
+		words.sort(comparator);
+		System.out.println(words);
+
+		// old way to print each element from a collection
+		for (String word : words) {
+			System.out.println(word);
+		}
+
+		// iteration using Consumer interface (default method forEach on Iterable)
+		words.forEach(new PrintWord());
+	}
+}
+
+class PrintWord implements Consumer<String> {
+	@Override
+	public void accept(String word) {
+		System.out.println(word);
+	}
+}
+
+class SizeComparator implements Comparator<String> {
+	@Override
+	public int compare(String o1, String o2) {
+		if (o1.length() < o2.length())
+			return -1;
+		if (o1.length() > o2.length())
+			return 1;
+		return 0;
+	}
+
+}
