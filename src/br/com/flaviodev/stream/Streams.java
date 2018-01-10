1324309963521b5fb245893e3d67e5875c5a5a9e
flavio <fdsdev@gmail.com>
2018-01-10 00:51:30 -0200

    parallel stream

--- src/br/com/flaviodev/stream/Streams.java
+++ src/br/com/flaviodev/stream/Streams.java
@@ -3,8 +3,6 @@ package br.com.flaviodev.stream;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 class Course {
