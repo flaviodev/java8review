76f832ede0607ed758e46e7c6e8eb564f5a832d9
flavio <fdsdev@gmail.com>
2018-01-10 00:06:26 -0200

    using stream: filter, map, mapToInt, sum

---
+++ src/br/com/flaviodev/stream/Streams.java
@@ -0,0 +1,58 @@
+package br.com.flaviodev.stream;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Course {
+
+	private String name;
+	private int amountOfStudents;
+
+	public Course(String name, int amountOfStudents) {
+		this.name = name;
+		this.amountOfStudents = amountOfStudents;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAmountOfStudents() {
+		return amountOfStudents;
+	}
+}
+
+public class Streams {
+
+	public static void main(String[] args) {
+
+		List<Course> courses = new ArrayList<>();
+		courses.add(new Course("Java 8", 100));
+		courses.add(new Course("Kotlin", 80));
+		courses.add(new Course("Ionic 3", 50));
+		courses.add(new Course("React", 300));
+
+		courses.sort(Comparator.comparing(Course::getAmountOfStudents));
+
+		courses.forEach(c -> System.out.println(c.getName() + " - " + c.getAmountOfStudents()));
+
+		// using stream for filtering
+		courses.stream()
+			.filter(c -> c.getAmountOfStudents() > 90)
+			.forEach(c -> System.out.println(c.getName()));
+
+		// using stream for mapping data
+		courses.stream()
+			.filter(c -> c.getAmountOfStudents() > 90)
+			.map(c -> c.getAmountOfStudents())
+			.forEach(System.out::println);
+		
+		// using stream for summing data
+		int sum = courses.stream()
+			.filter(c -> c.getAmountOfStudents() > 90)
+			.mapToInt(c -> c.getAmountOfStudents()).sum();
+	
+		System.out.println(sum);
+	}
+}
